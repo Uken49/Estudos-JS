9eeaf3b71e38a940c4ca416b410e76edd75735ce
Uken49 <heldergavioes@hotmail.com>
2021-12-22 22:28:56 -0300

    20/12/2021

---
+++ exercicio15.js
@@ -0,0 +1,30 @@
+/*
+var c = 1
+do{
+    console.log(`Laço ${c}`)
+    c++
+}while (c <= 6)
+*/
+//---------------------------------------------------------
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Laço ${c}`)
+    c++
+}
+*/
+//---------------------------------------------------------
+console.log('Vai começar...')
+for(var c = 1; c <=5; c++){
+    console.log(`Laço ${c}`)
+}
+console.log('Fim!')
+
+/*
+console.log('Laço 1')
+console.log('Laço 2')
+console.log('Laço 3')
+console.log('Laço 4')
+console.log('Laço 5')
+console.log('Laço 6')
+*/
