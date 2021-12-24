8b2ef928e7b7e0e61105d2da54be0a11cac4f565
Uken49 <heldergavioes@hotmail.com>
2021-12-23 23:59:16 -0300

    23/12/2021

---
+++ exercicio16/script.js
@@ -0,0 +1,24 @@
+function contar() {
+    var inicio = document.querySelector('input#txInicio')
+    var fim = document.querySelector('input#txFim')
+    var passo = document.querySelector('input#txPasso')
+    var res = document.querySelector('div#res')
+
+    if (inicio.value == '' || fim.value == '' || passo.value == '') {
+        alert('Preencha todos os campos.')
+
+    }else if(fim.value < inicio.value){
+        alert('O campo "Fim" não pode ser menor que o campo "Inicio".')
+
+    }else{
+        res.innerHTML = ''
+        for (let i = inicio.value; i <= fim.value ; i=Number(i)+Number(passo.value)) {
+            res.innerHTML += `${i}`
+            if (i < fim.value++ ) {
+                res.innerHTML += String.fromCodePoint(128073);
+            }
+        }
+        res.innerHTML += String.fromCodePoint(127937);
+
+    }
+}
