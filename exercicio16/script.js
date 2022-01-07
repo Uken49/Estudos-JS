ada7acf83dc6a6fbb7117cd655c3c32edc0ca4e9
Uken49 <heldergavioes@hotmail.com>
2022-01-06 23:29:48 -0300

    06/01/2022

--- exercicio16/script.js
+++ exercicio16/script.js
@@ -1,23 +1,34 @@
 function contar() {
-    var inicio = document.querySelector('input#txInicio')
-    var fim = document.querySelector('input#txFim')
-    var passo = document.querySelector('input#txPasso')
-    var res = document.querySelector('div#res')
+    let inicio = document.querySelector('input#txInicio')
+    let fim = document.querySelector('input#txFim')
+    let passo = document.querySelector('input#txPasso')
+    let res = document.querySelector('div#res')
 
-    if (inicio.value == '' || fim.value == '' || passo.value == '') {
-        alert('Preencha todos os campos.')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Preencha todos os campos.'
 
-    }else if(fim.value < inicio.value){
-        alert('O campo "Fim" não pode ser menor que o campo "Inicio".')
+    } else if (Number(fim.value) < Number(inicio.value)) {
+        res.innerHTML = 'O campo "Fim" não pode ser menor que o campo "Inicio".'
 
-    }else{
-        res.innerHTML = ''
-        for (let i = inicio.value; i <= fim.value ; i=Number(i)+Number(passo.value)) {
-            res.innerHTML += `${i}`
-            if (i < fim.value++ ) {
-                res.innerHTML += String.fromCodePoint(128073);
+    } else {
+        res.innerHTML = 'Contando:<br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1.')
+            p = 1
+        }
+        if (i < f) {
+            for (let j = i; i <= f; j += p) {
+                res.innerHTML += `${j} \u{1F449}`
+            }
+        }else{
+            for(let j=i; j>=f; c-=p){
+                res.innerHTML += `${j} \u{1F449}`
             }
         }
-        res.innerHTML += String.fromCodePoint(127937);
+        res.innerHTML += `\u{1F3c1}`
+
     }
 }
